223f1f0c9c69ab1b8cd2b76fd8bfa6837d89b222
RockdrigoTC <torrealbacruzrodrigo@gmail.com>
2025-05-06 00:39:21 -0600

    Fix: lazyvim to lazywal

--- lua/lazywal/init.lua
+++ lua/lazywal/init.lua
@@ -1,25 +1,25 @@
 -- LazyWal.vim/lua/lazywal/init.lua
-local config = require("lazyvim.config")
+local config = require("lazywal.config")
 
 local M = {}
 ---@type {light?: string, dark?: string}
 M.styles = {}
 
----@param opts? lazyvim.Config
+---@param opts? lazywal.Config
 function M.load(opts)
-  opts = require("lazyvim.config").extend(opts)
+  opts = require("lazywal.config").extend(opts)
   local bg = vim.o.background
   local style_bg = opts.style == "day" and "light" or "dark"
 
   if bg ~= style_bg then
-    if vim.g.colors_name == "lazyvim-" .. opts.style then
+    if vim.g.colors_name == "lazywal-" .. opts.style then
       opts.style = bg == "light" and (M.styles.light or "day") or (M.styles.dark or "night")
     else
       vim.o.background = style_bg
     end
   end
   M.styles[vim.o.background] = opts.style
-  return require("lazyvim.theme").setup(opts)
+  return require("lazywal.theme").setup(opts)
 end
 
 M.setup = config.setup
